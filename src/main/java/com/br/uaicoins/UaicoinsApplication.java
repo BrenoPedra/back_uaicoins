0f3523bffa03398475f5a59de9a3fbeb7e271bb6
itallocorrea <32783253+itallocorrea@users.noreply.github.com>
2019-05-23 03:51:18 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/br/uaicoins/UaicoinsApplication.java
@@ -0,0 +1,13 @@
+package com.br.uaicoins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaicoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaicoinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/uaicoins/UaicoinsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.br.uaicoins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UaicoinsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
